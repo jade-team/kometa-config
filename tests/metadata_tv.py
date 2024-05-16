977f01337b0549f060bb129182239c6b5245d7e5
Daniel Martin Gonzalez <danimart1991@outlook.com>
2024-05-16 11:05:08 +0200

    ✨ TV: Add Metadata TV with Grey's Anatomy (#216)

--- tests/metadata_tv.py
+++ tests/metadata_tv.py
@@ -2,7 +2,9 @@ import logging
 import yaml
 import os
 
-url_poster_base = "https://github.com/jade-team/kometa-config/raw/main/metadata/movies/assets/"
+url_poster_base = (
+    "https://github.com/jade-team/kometa-config/raw/main/metadata/tv/assets/"
+)
 file_dir = os.path.dirname(os.path.realpath("__file__"))
 
 
@@ -20,44 +22,44 @@ def run() -> int:
     logging.info("=" * 50)
     logging.error = CallCounted(logging.error)
 
-    with open(os.path.join(file_dir, "./metadata/movies/movies.yml"), "r") as file:
+    with open(os.path.join(file_dir, "./metadata/tv/tv.yml"), "r") as file:
         prime_service = yaml.safe_load(file)
-        assets = os.listdir(os.path.join(file_dir, "./metadata/movies/assets"))
+        assets = os.listdir(os.path.join(file_dir, "./metadata/tv/assets"))
 
         logging.info(
-            f"Testing Metadata - Movies over {len(prime_service['metadata'])} items and {len(assets)} assets"
+            f"Testing Metadata - TV Shows over {len(prime_service['metadata'])} items and {len(assets)} assets"
         )
 
-        for movie_id, movie_data in prime_service["metadata"].items():
-            if not movie_data:
-                logging.error(f"Movie {movie_id} fields are missing")
+        for tv_id, tv_data in prime_service["metadata"].items():
+            if not tv_data:
+                logging.error(f"TV Show {tv_id} fields are missing")
             else:
-                title = movie_data.get("title", None)
-                url_poster = movie_data.get("url_poster", None)
+                title = tv_data.get("title", None)
+                url_poster = tv_data.get("url_poster", None)
 
                 if not title:
-                    logging.error(f"Movie {movie_id} title field is missing")
+                    logging.error(f"TV Show {tv_id} title field is missing")
                 if not url_poster:
                     logging.error(
-                        f"Movie {movie_id} - {title} url_poster field is missing"
+                        f"TV Show {tv_id} - {title} url_poster field is missing"
                     )
                 if url_poster:
                     if not url_poster.startswith(url_poster_base):
                         logging.error(
-                            f"Movie {movie_id} - {title} has an invalid url_poster base uri"
+                            f"TV Show {tv_id} - {title} has an invalid url_poster base uri"
                         )
                     poster_file = url_poster[len(url_poster_base) :]
-                    if not poster_file.startswith(str(movie_id)):
+                    if not poster_file.startswith(str(tv_id)):
                         logging.error(
-                            f"Movie {movie_id} - {title} has a different TMDB Id and url_poster TMDB Id ({poster_file})"
+                            f"TV Show {tv_id} - {title} has a different TMDB Id and url_poster TMDB Id ({poster_file})"
                         )
                     if not url_poster.endswith(".jpg"):
                         logging.error(
-                            f"Movie {movie_id} - {title} has an invalid url_poster extension (.jpg)"
+                            f"TV Show {tv_id} - {title} has an invalid url_poster extension (.jpg)"
                         )
                     if not poster_file in assets:
                         logging.error(
-                            f"Movie {movie_id} - {title} url_poster ({poster_file}) NOT found in assets folder"
+                            f"TV Show {tv_id} - {title} url_poster ({poster_file}) NOT found in assets folder"
                         )
                     else:
                         assets.remove(poster_file)
